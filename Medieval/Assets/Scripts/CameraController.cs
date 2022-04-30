9aa7672b3032c771db2a6ace83334a0ddfbed1a8
Q9E <78754510+Q9E@users.noreply.github.com>
2022-04-30 17:14:23 +0100

    not very sucessful
    a

--- Medieval/Assets/Scripts/CameraController.cs
+++ Medieval/Assets/Scripts/CameraController.cs
@@ -6,23 +6,40 @@ using UnityEngine.InputSystem;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private float panSpeed;
+    private Controls controls;
+    private readonly Vector3 pos; //Unity done readonly, if it works it works :shrug:
+
+    private void Awake()
+    {
+        controls = new Controls();
+
+        controls.Player.Place.performed += context => CameraNorth(pos);
+        controls.Player.Place.canceled += context => StopCamera(pos);
+    }
 
     void Update()
     {
-        Vector3 pos = transform.position;
+
     }
 
-    private void CameraNorth(InputAction.CallbackContext context, Vector3 pos)
+    private void CameraNorth(Vector3 pos)
     {
-        if(context.performed)
-        {
-            Debug.Log("Performed");
-            pos.y += panSpeed * Time.deltaTime;
-        }
-        if(context.canceled)
-        {
-            Debug.Log("Cancelled");
-            pos.y = 0;
-        }
+        pos.y += panSpeed * Time.deltaTime;
+        Debug.Log("North");
+    }
+
+    private void StopCamera(Vector3 pos)
+    {
+        pos.y = 0;
+    }
+
+    private void OnEnable()
+    {
+        controls.Player.CameraNorth.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.CameraNorth.Disable();
     }
 }
